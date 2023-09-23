603051503261a327da50c422e8bba8f96bf2e189
tracer-ysyx <tracer@ysyx.org>
2023-09-23 16:33:35 +0800

    > compile NEMU ysyx_22040000 张三 Linux xiangui-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:35 up  5:47,  1 user,  load average: 0.99, 1.36, 1.11

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -92,7 +92,7 @@ static int cmd_x(char *args)
 }
 
 static int cmd_p(char *args){
-  bool success = 0;
+  bool success = true;
   expr(args, &success);
   printf("success:%d\n",success);
   return 0;
