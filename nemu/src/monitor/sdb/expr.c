ccccdd6abb1db44d0f9a06a9addb6f7aacbaa171
tracer-ysyx <tracer@ysyx.org>
2023-10-02 15:41:00 +0800

    > compile NEMU ysyx_22040000 张三 Linux xglab-virtual-machine 5.15.0-84-generic #93~20.04.1-Ubuntu SMP Wed Sep 6 16:15:40 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:41:00 up  2:14,  1 user,  load average: 0.21, 0.26, 0.23

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -41,7 +41,7 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
-  {"!=", TK_NEQ}, 
+  {"\\!=", TK_NEQ}, 
   {"&&", TK_ADD},
   {"\\-", '-'},         // minus
   {"\\*", '*'},         // multiplication
