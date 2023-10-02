60b8eb4775176bccf67a3bcec4203372b5119ab8
tracer-ysyx <tracer@ysyx.org>
2023-10-02 23:00:00 +0800

    > compile NEMU ysyx_22040000 张三 Linux xglab-virtual-machine 5.15.0-84-generic #93~20.04.1-Ubuntu SMP Wed Sep 6 16:15:40 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:00:00 up  9:33,  1 user,  load average: 0.36, 0.35, 0.20

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -21,7 +21,7 @@ void init_wp_pool() {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
     wp_pool[i].expr = 1;
-    wp_pool[i].expr_str = '\0';
+    wp_pool[i].expr_str = "\0";
   }
 
   head = NULL;
