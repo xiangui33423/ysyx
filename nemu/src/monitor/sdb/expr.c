803920c4faba3193b808e7375f9aa49531077311
tracer-ysyx <tracer@ysyx.org>
2023-10-12 21:58:06 +0800

    > compile NEMU ysyx_22040000 张三 Linux xglab-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:58:06 up 12:51,  1 user,  load average: 0.50, 0.50, 0.32

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -103,12 +103,11 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-        
+        strcpy(tokens[nr_token].str,"");
         switch (rules[i].token_type) {
           case '+': tokens[nr_token].type = '+';  
                     strncpy(tokens[nr_token++].str, substr_start, substr_len);
                     break;
-
           case '-':   tokens[nr_token].type = '-';   
                       strncpy(tokens[nr_token++].str, substr_start, substr_len);
                       break;                    
