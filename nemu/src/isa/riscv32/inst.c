082b5b71677da8d2b54c6f0393266b10e6df7597
tracer-ysyx <tracer@ysyx.org>
2023-10-29 22:57:00 +0800

    > compile NEMU ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:57:00 up  8:16,  1 user,  load average: 0.39, 0.42, 0.44

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -43,7 +43,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
-    case TYPE_J:                   immJ(); s->pc += *imm; break;
+    case TYPE_J:                   immJ(); s->pc += (*imm%4 == 2 ? *imm + 2 : *imm); break;
     case TYPE_R: src1R(); src2R();         break;
   }
 }
