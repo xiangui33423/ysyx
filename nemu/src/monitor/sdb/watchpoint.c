ee92dc555e091aa6be9edef57918e31d35f918f9
tracer-ysyx <tracer@ysyx.org>
2023-11-02 14:42:38 +0800

    > compile NEMU ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:42:38 up 20 min,  1 user,  load average: 0.27, 0.58, 1.22

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -124,7 +124,7 @@ bool watch_all()
         has_print_head = true;
       }
 
-      printf("%-6d\t%-20s\t%-14x\t%-14x\n",tmp->NO,tmp->expr,res,tmp->res);
+      printf("%-6d\t%-20s\t0x%-14x\t0x%-14x\n",tmp->NO,tmp->expr,res,tmp->res);
       tmp->res = res;
       return true;
     }
