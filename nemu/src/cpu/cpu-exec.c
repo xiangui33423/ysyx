f4d70a84d79bde69b9dd91f11120e316585a4d7a
tracer-ysyx <tracer@ysyx.org>
2023-11-03 16:16:47 +0800

    > compile NEMU ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:16:47 up  2:32,  0 users,  load average: 0.30, 0.22, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -30,7 +30,6 @@ CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
-static char ringbuf[17][128];
 
 void device_update();
 
@@ -40,6 +39,14 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+
+#ifdef CONFIG_TRACE_IRINGBUF
+  char ringbuf[17][];
+  if(nemu_state.state == NEMU_ABORT)
+  {
+    
+  }
+#endif
 
 #ifdef CONFIG_WATCHPOINT
   if(watch_all() == true) nemu_state.state = NEMU_STOP;
@@ -83,6 +90,7 @@ static void execute(uint64_t n) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
+    
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
