e258f2f27c046b32e0a78df8628b5b882391ef06
tracer-ysyx <tracer@ysyx.org>
2023-11-09 15:34:52 +0800

    > compile NEMU ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:34:52 up  4:56,  1 user,  load average: 0.77, 0.62, 0.59

--- npc/vsrc/ysyx_23060096_npc.v
+++ npc/vsrc/ysyx_23060096_npc.v
@@ -6,7 +6,10 @@ module ysyx_23060096_npc (
 );
 
 //========IF=======
-ysyx_23060096_Reg u_pc_reg #(32,0) (clk,rstn,pc+32'd4,pc);
+ysyx_23060096_Reg u_pc_reg #(32,32'h8000_0000) (clk,rstn,pc+32'h4;,pc);
+
+//========ID=======
+
 
 
 endmodule //ysyx_23060096_NPC
