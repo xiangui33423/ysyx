24936d2c3d8b9c484c301e243f4590183069395e
tracer-ysyx <tracer@ysyx.org>
2023-11-11 20:51:14 +0800

    > sim RTL ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:51:14 up  3:03,  1 user,  load average: 0.88, 0.84, 0.79

--- npc/vsrc/ysyx_23060096_IFU.v
+++ npc/vsrc/ysyx_23060096_IFU.v
@@ -1,4 +1,4 @@
-module ysyx_23060096_PCreg (
+module ysyx_23060096_IFU (
     input         clk,
     input         rst_n,
     input  [31:0] pc,
@@ -10,7 +10,7 @@ wire [31:0] addr;
 assign addr = (pc - 32'h8000_0000) >> 2;
     reg [31:0] inst_reg [1<<32-1:0];
 
-    always @(posedge clk ) begin
+    always @(posedge clk) begin
         if(!rst_n) begin
             inst_reg[0] <= 32'b0000000_01111_00000_000_00001_0010011;
             inst_reg[1] <= 32'b0000000_01111_00000_000_00011_0010011;
---
+++ npc/vsrc/ysyx_23060096_ImmGen.v
@@ -0,0 +1,31 @@
+module ysyx_23060096_ImmGen (
+    input  clk,
+    input  rstn,
+    input  [24:0] inst,
+    input  [2:0]  ExtOP,
+    output [32:0] imm
+);
+
+    wire [31:0] immI;
+    wire [31:0] immU;
+    wire [31:0] immS;
+    wire [31:0] immB;
+    wire [31:0] immJ;
+
+assign immI = {{20{inst[24]}},inst[23:12]};
+assign immU = {inst[24:5], 12'b0};
+assign immS = {{20{inst[24]}}, inst[24:15], inst[4:0]};
+assign immB = {{20{inst[24]}}, inst[0], inst[23:18], inst[4:1], 1'b0};
+assign immJ = {{12{inst[24]}}, inst[12:5], inst[13], inst[23:14], 1'b0};
+
+always @(*) begin
+    case(ExtOP)
+        3'b000: imm = immI;
+        3'b001: imm = immU;
+        3'b010: imm = immS;
+        3'b011: imm = immB;
+        3'b100: imm = immJ;
+    endcase
+end
+
+endmodule //ysyx_23060096_ImmGen
