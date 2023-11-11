fab5c09e57791a8bc533ca8178f9484cc0edc3f2
tracer-ysyx <tracer@ysyx.org>
2023-11-11 20:01:56 +0800

    > sim RTL ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:01:56 up  2:14,  1 user,  load average: 0.78, 0.76, 0.72

--- npc/vsrc/ysyx_23060096_npc.v
+++ npc/vsrc/ysyx_23060096_npc.v
@@ -6,7 +6,7 @@ module ysyx_23060096_npc (
 );
 
 //========IF=======
-ysyx_23060096_Reg u_pc_reg #(32,32'h8000_0000) (clk,rstn,pc+32'h4;,pc);
+// ysyx_23060096_Reg u_pc_reg #(32,32'h8000_0000) (clk,rstn,pc+32'h4;,pc);
 
 //========ID=======
 
