95c336e3ea2561b91490ce726f51521b1cc19333
tracer-ysyx <tracer@ysyx.org>
2023-11-11 17:49:14 +0800

    > sim RTL ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:49:14 up 35 min,  1 user,  load average: 0.28, 0.51, 0.63

--- npc/vsrc/ysyx_23060096_RegisterFile.v
+++ npc/vsrc/ysyx_23060096_RegisterFile.v
@@ -14,12 +14,13 @@ module ysyx_23060096_RegisterFile #(ADDR_WIDTH = 32, DATA_WIDTH = 5) (
 
   //=====initial======
   integer i;
-  if(!rstn) 
-    for (i = 0;i < 2^ADDR_WIDTH; i = i + 1) begin
-      rf = 0;
-  end
+
 
   always @(posedge clk) begin
+    if(!rstn) 
+    for (i = 0;i < 2^ADDR_WIDTH; i = i + 1) begin
+      rf = 0;
+    end
     if (w_en) rf[waddr] <= wdata;
   end
   assign busA = rf[Ra];
