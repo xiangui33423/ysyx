90dc6bea80805736f4cacd01fbd74d5f76b94164
tracer-ysyx <tracer@ysyx.org>
2023-11-11 19:52:35 +0800

    > sim RTL ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:52:35 up  2:05,  1 user,  load average: 0.68, 0.71, 0.65

---
+++ npc/vsrc/ysyx_23060096_PCreg.v
@@ -0,0 +1,8 @@
+module ysyx_23060096_PCreg (
+    input         clk,
+    input         rst_n,
+    input  [31:0] pc,
+    input  []
+);
+
+endmodule //ysyx_23060096_PCreg
---
+++ npc/vsrc/ysyx_23060096_pc.v
@@ -0,0 +1,19 @@
+module ysyx_23060096_pc (
+    input         clk,
+    input         rstn,
+    output [31:0] pc
+);
+    reg [31:0] pc_next;
+    always @(posedge clk) begin
+      if(!rstn)
+        begin
+          pc_next <= 32'h8000_0000;
+        end
+        else begin
+          pc_next <= pc_next + 32'h4; 
+        end
+    end
+
+    assign pc = pc_next;
+
+endmodule //ysyx_23060096_pc
