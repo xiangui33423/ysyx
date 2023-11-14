d3b9305ad84902858255cfa3ed1df15772843189
tracer-ysyx <tracer@ysyx.org>
2023-11-14 18:00:09 +0800

    > sim RTL ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:00:09 up  4:45,  1 user,  load average: 0.52, 0.46, 0.38

--- npc/vsrc/ysyx_23060096_RegisterFile.v
+++ npc/vsrc/ysyx_23060096_RegisterFile.v
@@ -27,9 +27,6 @@ module ysyx_23060096_RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 32) (
       end
   end
 
-  always @(posedge clk ) begin
-    
-  end
   assign busA = rf[Ra];
   assign busB = rf[Rb];
 endmodule
