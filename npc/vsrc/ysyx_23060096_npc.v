2512a5b3a2bae2df51972658c1885b20a4e1fc5b
tracer-ysyx <tracer@ysyx.org>
2023-11-14 17:05:00 +0800

    > sim RTL ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:05:00 up  3:50,  1 user,  load average: 0.21, 0.33, 0.35

--- npc/vsrc/ysyx_23060096_npc.v
+++ npc/vsrc/ysyx_23060096_npc.v
@@ -14,11 +14,11 @@ ysyx_23060096_pc u_pc(
 
 //========ID=======
 wire [2:0] ExtOP,
-reg        RegWr;
+wire       RegWr;
 wire       ALUAsrc;
 wire [1:0] ALUBsrc;
 wire [2:0] Branch,
-reg  [3:0] ALUctr;
+wire [3:0] ALUctr;
 wire       MemtoReg;
 wire       MemWr;
 wire [2:0] MemOP;
