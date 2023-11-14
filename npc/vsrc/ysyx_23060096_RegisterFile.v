1b5a36c59938e94cf99c933608c649a7acdde30f
tracer-ysyx <tracer@ysyx.org>
2023-11-14 16:59:42 +0800

    > sim RTL ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:42 up  3:44,  1 user,  load average: 0.37, 0.51, 0.40

--- npc/vsrc/ysyx_23060096_RegisterFile.v
+++ npc/vsrc/ysyx_23060096_RegisterFile.v
@@ -8,8 +8,8 @@ module ysyx_23060096_RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 32) (
   input [ADDR_WIDTH-1:0] waddr, 
   input w_en,
 
-  output reg [DATA_WIDTH-1:0] busA,
-  output reg [DATA_WIDTH-1:0] busB
+  output [DATA_WIDTH-1:0] busA,
+  output [DATA_WIDTH-1:0] busB
 );
   reg [DATA_WIDTH-1:0] rf [1<<ADDR_WIDTH-1:0];
 
@@ -18,19 +18,18 @@ module ysyx_23060096_RegisterFile #(ADDR_WIDTH = 5, DATA_WIDTH = 32) (
   always @(negedge clk) begin
     if(!rstn) begin
       for (i = 0;i < 1<<ADDR_WIDTH; i = i + 1) begin
-        rf[i] = 32'h0;
+        rf[i] <= 32'h0;
         end
     end
     else
       if (w_en) begin
-        rf[waddr] = wdata;
+        rf[waddr] <= wdata;
       end
   end
 
   always @(posedge clk ) begin
-    if(rstn)begin
-      busA = rf[Ra];
-      busB = rf[Rb];
-    end
+    
   end
+  assign busA = rf[Ra];
+  assign busB = rf[Rb];
 endmodule
