c000e30d0f23338b16ad20c4e6b6d50911a3b2a9
tracer-ysyx <tracer@ysyx.org>
2023-11-19 18:34:58 +0800

    > sim RTL ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:34:58 up 18 min,  1 user,  load average: 0.22, 0.42, 0.50

--- npc/csrc/npc.cpp
+++ npc/csrc/npc.cpp
@@ -38,7 +38,7 @@ void sim_exit()
     tfp->close();
 }
 
-extern "C" void ebreak(int inst)
+void ebreak(int inst)
 {
     if(inst == 0b0000000_00001_00000_000_00000_11100_11)
         contextp->gotFinish() = 1;
