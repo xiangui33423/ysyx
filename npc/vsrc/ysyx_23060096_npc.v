7b2c4008896a88caa6adb518c47031b9468d6482
tracer-ysyx <tracer@ysyx.org>
2023-11-19 18:22:24 +0800

    > sim RTL ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:22:24 up 6 min,  1 user,  load average: 0.85, 0.96, 0.54

--- npc/vsrc/ysyx_23060096_npc.v
+++ npc/vsrc/ysyx_23060096_npc.v
@@ -14,13 +14,13 @@ ysyx_23060096_pc u_pc(
 
 import "DPI-C" function int ebreak(int inst);
 //========ID=======
-// reg en;
-// reg clk_en;
-// always @(*) begin
-//     if(inst == 32'b0000000_00001_00000_000_00000_11100_11) begin
-//         clk_en = en & clk;
-//     end
-// end
+reg en;
+reg clk_en;
+always @(*) begin
+    if(inst == 32'b0000000_00001_00000_000_00000_11100_11) begin
+        clk_en = en & clk;
+    end
+end
 
 ebreak(inst);
 
