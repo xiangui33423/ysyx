5418940eec827112af52b8b37c2a6e5a54bf4493
tracer-ysyx <tracer@ysyx.org>
2023-11-24 15:53:47 +0800

    > compile NEMU ysyx_22060096 姚文涛 Linux xiangui-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:53:47 up  3:23,  0 users,  load average: 1.08, 1.19, 1.08

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -54,7 +54,7 @@ word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr)))
   {
     #ifdef CONFIG_MTRACE
-      Log(" \naddress = " FMT_PADDR " is\n at pc = " FMT_WORD,  addr, cpu.pc);
+      Log(" \naddress = " FMT_PADDR " \n     pc = " FMT_WORD,  addr, cpu.pc);
     #endif
     return pmem_read(addr, len);
   } 
