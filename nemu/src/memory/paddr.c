b220d00edb0d9261b0cfd32e3e2ecc178d6c5217
tracer-ysyx <tracer@ysyx.org>
2023-11-25 20:38:33 +0800

    > compile NEMU ysyx_22060096 姚文涛 Linux xiangui-System-Product-Name 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:38:33 up  1:03,  1 user,  load average: 4.87, 3.82, 3.48

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -62,7 +62,7 @@ word_t paddr_read(paddr_t addr, int len) {
     return pmem_read(addr, len);
   } 
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  out_of_bound(addr);
+  // out_of_bound(addr);
   return 0;
 }
 
@@ -77,5 +77,5 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     pmem_write(addr, len, data); return; 
   }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  out_of_bound(addr);
+  // out_of_bound(addr);
 }
