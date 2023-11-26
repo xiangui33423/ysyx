c054a84eb1fd46268a97646a9aaf5beeef7de07f
tracer-ysyx <tracer@ysyx.org>
2023-11-26 22:24:51 +0800

    > compile NEMU ysyx_22060096 姚文涛 Linux xiangui-System-Product-Name 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:24:51 up  1:22,  1 user,  load average: 0.25, 0.26, 0.25

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,7 +26,7 @@ $(BINARY):: compile_git
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
-override ARGS += -e=$(BUILD_DIR)/dummy-riscv32-nemu.elf
+override ARGS += --elf=$(BUILD_DIR)/dummy-riscv32-nemu.elf
 
 # Command to execute NEMU
 IMG ?=
