8a17c2d41f10fff6e553ce5d41872bd2de1ebf50
tracer-ysyx <tracer@ysyx.org>
2023-12-17 21:04:34 +0800

    > compile NEMU ysyx_22060096 姚文涛 Linux xiangui-System-Product-Name 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:04:34 up  7:58,  3 users,  load average: 1.90, 2.10, 2.07

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -88,7 +88,8 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     
     if(cpu.pc >= func[i].value && cpu.pc < func[i].value + func[i].size)
     {
-      if(BITS(_this->isa.inst.val,6,0) == 0b1101111)
+      // if(BITS(_this->isa.inst.val,6,0) == 0b1101111)
+      if(cpu.pc == func[i].value)
         printf("call:0x%x  %s\n",cpu.pc,func[i].name);
       if(BITS(_this->isa.inst.val,6,0) == 0b1100111)
         printf("ret:0x%x  %s\n",cpu.pc,func[i].name);
