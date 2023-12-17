cc9fe7328ce070221df813d1ba2bea7d35408bbc
tracer-ysyx <tracer@ysyx.org>
2023-12-17 21:29:26 +0800

    > compile NEMU ysyx_22060096 姚文涛 Linux xiangui-System-Product-Name 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:29:26 up  8:23,  3 users,  load average: 0.99, 0.69, 1.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -89,14 +89,19 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     if(cpu.pc >= func[i].value && cpu.pc < func[i].value + func[i].size)
     {
       j = _this->isa.inst.val;
-      // if(BITS(_this->isa.inst.val,6,0) == 0b1101111)
-      if(BITS(j,6,0) == 0b1100111 && BITS(j, 19, 15) == 1 && BITS(j, 11, 7) == 0)
-        printf("      ret:0x%x  %s\n",cpu.pc,func[i].name);
-      else if(BITS(j,6,0) == 0b1100111 && BITS(j, 11, 7) < 2)
-        printf("call:0x%x  %s\n",cpu.pc,func[i].name);
-      else 
-      if(BITS(j,6,0) == 0b1101111 && BITS(j, 11, 7) == 1)
-        printf("call:0x%x  %s\n",cpu.pc,func[i].name);
+
+      if(BITS(j,6,0) == 0b1100111)
+      {
+        if(BITS(j, 19, 15) == 1 && BITS(j, 11, 7) == 0)
+          printf("      ret:0x%x  %s\n",cpu.pc,func[i].name);
+        else if(BITS(j, 11, 7) < 2)
+          printf("call:0x%x  %s\n",cpu.pc,func[i].name);
+      }
+      else if(BITS(j,6,0) == 0b1101111)
+      {
+        if(BITS(j, 11, 7) == 0) printf("call:0x%x  %s\n",cpu.pc,func[i].name);
+      }
+
       break;
     }
     // if(cpu.pc == func[i].value + func[i].size -4 && func[i].flag == 1)
